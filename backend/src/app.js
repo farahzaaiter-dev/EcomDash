a4d08f960a4216fcd744c19fc11f1638a7b991b2
farahzaaiter-dev <farahzaaiter@gmail.com>
2026-06-29 19:51:24 +0200

    Configure KPI routes in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const kpiRoutes = require("./routes/kpiRoutes");
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use("/api/kpis", kpiRoutes);
 
 module.exports = app;
